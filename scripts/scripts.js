e9ed3de3c38068dbe92b4f586b32baec5236aac6
KyleGoslan <kylegoslan@me.com>
2015-05-31 15:37:24 +0100

    Play/pause video on scroll

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,6 +4,13 @@
         height: 6000,                   // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
         onScroll: function(percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
             console.log(percent);
+
+            if(percent > 0.025) {
+				$(".video")[0].play();
+            } else {
+	            $(".video")[0].pause();
+            }
+
         }
     });
 
